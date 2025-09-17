0cd3ab6a71e3cce5323ed1d38ac93a8761c50820
Adriana Pereira77 <adriana_pereira152@estudante.sesisenai.org.br>
2025-09-16 22:10:33 -0300

    Aula do Luciano

---
+++ Professor Luciano/logica-js/arrays-objetos.js
@@ -0,0 +1,24 @@
+const alunosDoSenai = [
+    {
+        nome: "João",
+        email: "joao@gmail.com",
+        matricula: "123",
+        idade: 40
+    },
+    {
+        nome: "Maria",
+        email: "maria@gmail.com",
+        matricula: "321",
+        idade: 30
+    },
+    {
+        nome: "Adriana",
+        email: "adriana@gmail.com",
+        matricula: "123",
+        idade: 35
+    }
+]
+
+
+console.log(`o nome do usuário é ${alunosDoSenai[0].nome} e o nome do usuário é ${alunosDoSenai[1].nome} e o nome do usuário é ${alunosDoSenai[2].nome}`)
+
---
+++ Professor Luciano/logica-js/objetos.js
@@ -0,0 +1,15 @@
+const usuario = {
+login:"username",
+email:"email@gmail.com",
+saldo:50000.00,
+isLogin:true,
+senha:"12345678"
+
+}
+
+console.log(`O email do usuário é ${usuario.email} e o saldo do usuário é ${usuario.saldo}`)
+
+
+
+
+
