650098ba95d193d3cf8f8d67514675cf96e90634
Adriana Pereira77 <adriana_pereira152@estudante.sesisenai.org.br>
2025-09-22 22:15:53 -0300

    Aula de exercicios

---
+++ Professor Rafael/Exercicios/questão6.js
@@ -0,0 +1,26 @@
+ let pontuacao
+pontuacao = Number(prompt("Digite sua pontuação:"))
+
+// //if(pontuacao <= 10){
+//     alert("Deu ruim!")
+// }
+// if(pontuacao>10 && pontuacao<=100){
+// alert("Amanhã é um novo dia!")
+// }
+// if(pontuacao>100 && pontuacao<=200){
+//     alert("Supimpa!")
+// }else{
+//     alert("Mitou!")
+// }
+//versão errada a de cima
+
+if(pontuacao <= 10){
+    alert("Deu ruim!")
+}else if(pontuacao<=100){
+    alert("Amanhã é um novo dia!")
+}else if(pontuacao<=200){
+alert("Supimpa!")
+}else{
+    alert("Mitou")
+}
+
