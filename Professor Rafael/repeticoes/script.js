4bd4bc0af871e1679ca705bc7d35e3bc1933a7b9
Adriana Pereira77 <adriana_pereira152@estudante.sesisenai.org.br>
2025-09-29 22:16:18 -0300

    Repetições

---
+++ Professor Rafael/repeticoes/script.js
@@ -0,0 +1,23 @@
+
+// let acertos = Number(prompt("Quantos acertos?"))
+// let valorPorQuestao = 10/16
+// let nota = acertos * valorPorQuestao
+
+// alert("Sua nota: " + nota)
+
+
+
+// let contador = 0
+// while(contador <5){
+// alert("oi")
+// contador = contador + 1
+// }
+
+// while = significa enquanto
+
+
+let contador = 0
+while(contador < 2){
+    contador = contador + 1
+    console.log("Contador: " + contador)
+}
