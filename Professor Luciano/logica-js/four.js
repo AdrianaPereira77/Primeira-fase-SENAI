b149678883005a53d54250f80f0158a62ad1965e
Adriana Pereira77 <adriana_pereira152@estudante.sesisenai.org.br>
2025-09-30 22:14:55 -0300

    four funcao

---
+++ Professor Luciano/logica-js/four.js
@@ -0,0 +1,64 @@
+//for que imprima de 0 a 10
+
+// for(let i = 0; i<=10; i++){
+// console.log(i)
+// }
+
+//for que imprima de 10 a 0
+
+
+// for(let i = 10; i>=0; i--){
+// console.log(i)
+// }
+
+//++ operador de incremento e -- de decremento
+
+//São a mesma coisa:
+//numero += vai somar
+//numero = numero +3
+
+
+//imprima valores pares de 0 até 20
+
+// for(let i = 0; i<=20; i+=2){
+// console.log(i)
+// }
+
+// logica escrita i+=2 significa para pular de 2 em 2 assim vai sair só numeros pares 
+//= significa recebe
+
+// let soma = 0
+
+// for(let i = 1; i<100; i++){
+// soma += i
+// }
+
+// console.log("resultado da soma", soma)
+
+// let alunos = ["João","Maria","Joana"]
+// alunos.push("Pedro")
+
+// console.log(alunos.length)
+
+// quantidade de alunos ex length
+
+// let alunos = ["João","Maria","Joana"]
+// alunos.push("Pedro")
+
+// for(let i = 0; i<alunos.length; i++){
+//     console.log("aluno: ", alunos[i])
+// }
+
+
+
+//enquanto ex disso i<alunos.length (elemento dinâmico)
+
+//     let frutas = ["Banana","Maça","Laranja","Morango"]
+// frutas.push("Acerola") ----- essa linha é opcional n preisaria do push
+
+// for(let i = 0; i<frutas.length; i++){
+//     console.log("frutas: ", frutas[i])
+// }
+
+
+
---
+++ Professor Luciano/logica-js/funcoes.js
@@ -0,0 +1,28 @@
+// function saudacao(nome){
+// console.log(`Bom dia, ${nome}`)
+// }
+
+// saudacao("Adriana!")
+// saudacao("Marcos!")
+
+
+//bloco de código - permite reutilizar o function - permite o agrupamento na mesma função, mudando parametros () assim é sem parametros 
+// - declaração de variável é importante - string é o texto - isso significa => (arrow function)
+
+// const saudacao = (nome)=>{
+//     console.log(`Bom dia, ${nome}`)
+// }
+
+// saudacao("Adriana!")
+// saudacao("Marcos!")
+
+
+// const soma =  (x,y) => x+y 
+
+const soma = (x,y) =>{
+    // const subtracao = (x,y) => x-y
+return x+y;
+}
+console.log(`a soma é ${soma(3,5)}`) 
+
+
