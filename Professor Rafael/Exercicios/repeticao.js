b7e89cea4904ed526d96ddad3952ce5eb79df769
Adriana Pereira77 <adriana_pereira152@estudante.sesisenai.org.br>
2025-10-06 22:02:51 -0300

    ex repeicao

---
+++ Professor Rafael/Exercicios/repeticao.js
@@ -0,0 +1,100 @@
+//Faça um programa que calcule o quadrado de cada número de 1 a 10. (1)
+//🐱‍🏍Imprima os números de 10 a 1 em ordem decrescente. (2)
+//🐱‍🏍Calcule a soma dos números de 1 a 100. (3)
+
+// let i = 0
+// while(i<3){
+//     alert("Valor do i: " + i)
+//     i++
+// }
+
+// for(let i = 0; i<3; i++){
+//     alert("Valor do i: " + i)
+// }
+
+// let soma = 0
+// for(let i=1;i<11;i++){
+//     let nota = Number(prompt(`Digite a ${i} nota: `))
+//     soma = soma + nota 
+// }
+
+//  alert("Soma: " + soma)
+
+//  const nomes = ["Dona Bete", "Zezinho", "Mariazinha", "Joao", "Justin"]
+
+//  for(let i =0;i<4;i++){
+//      alert(nomes[i])
+//  }
+
+// EX 01 
+
+// for(let i = 1;i<10;i++){
+//      alert(`O quadrado de ${i} é ${i*i}`)
+// }   
+
+// EX 02
+// for(let i = 10;i>0;i--){
+//     alert(i)
+// }
+
+// EX 03 nn entendi 
+
+// primeira forma correta ex 3
+
+// let soma = 0   (sempre se usa 0 aqui)
+
+// for(let i=1; i<=100; i++){
+// console.log(i);
+// }
+
+// segunda forma correta ex 3
+// let i=1
+// while(i<=100){
+//     console.log(i);
+//     i++
+// }
+
+
+// EX 04 Imprima a tabuada do 5.
+
+// for(let i=1; i<=10;i++){
+// let resultado = i * 5
+// console.log(i + "x" + 5 + "=" + resultado);
+// }
+
+
+//EX 05 Altere o programa anterior para fornecer a tabuada do número que o usuário pedir.
+// let tabuada = prompt("Digite um número inteiro")
+// for(let i=1; i<=10;i++){
+// let resultado = i * tabuada
+// console.log(i + "x" + tabuada + "=" + resultado);
+// }
+
+//ex6
+// for (let i = 1; i <= 20; i++) {
+// if(i%2 == 1){
+//     alert(i)
+// }
+// }
+
+// 8 Calcule o fatorial de um número dado
+
+// let n = Number(prompt("Digite um número:"));
+// let fatorial = 1;
+
+// for (let i = 1; i <= n; i++) {
+//   fatorial *= i;
+// }
+
+// alert(`O fatorial de ${n} é ${fatorial}`); 
+
+
+//9!  9x8x7x6x5x4x3x2x1 (exemplo)
+
+
+
+
+
+
+
+
