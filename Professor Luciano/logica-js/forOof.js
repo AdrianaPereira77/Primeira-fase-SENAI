7cdb0d22bd167ff9ca830a0293b94353e27b2d1d
Adriana Pereira77 <adriana_pereira152@estudante.sesisenai.org.br>
2025-10-07 22:09:37 -0300

    aula filter e maps

---
+++ Professor Luciano/logica-js/forOof.js
@@ -0,0 +1,26 @@
+// const nomes = ["João", "Maria", "Ricardo", "Joana"]
+
+// for(let nome of nomes){
+//     console.log("Olá, " + nome + "!")
+// }
+
+//nesse jeito de cima, o console log puxa item por itém 
+//Olá, João!
+// Olá, Maria!
+// Olá, Ricardo!
+// Olá, Joana!
+
+
+// function renderizaArray(items){
+//     for(let item of items){
+//         console.log(item)
+//     }
+// }
+
+// let dados = [20.00,50,60,70]    //aceita qualquer tipo de dados desse jeito (numeros flutuantes e inteiros)
+// let animais = ["cachorro", "gato", "vaca", "galinha"]
+
+// renderizaArray(animais) //essa parte que define no (xxx) o que vai aparecer - ele recebe qualquer let ex dados ou animais 
+
+
+
---
+++ Professor Luciano/logica-js/map.js
@@ -0,0 +1,59 @@
+// const numeros = [3,5,8,10]
+// const valoresDobrados = numeros.map((valor) => valor*2)
+// console.log(numeros)
+// console.log(valoresDobrados)
+
+
+// const usuarios = [
+// {nome:"João", idade:32},
+// {nome:"Maria", idade:15},
+// {nome:"Pedro", idade:21}
+// ]
+// const nomesUsuarios = usuarios.map(usuario => usuario.nome)
+// console.log(nomesUsuarios)
+
+
+//crie um novo array que multiplique cada um deles por dois ex: 
+// const numeros = [3,5,8,10]
+// const valoresDobrados = numeros.map((valor) => valor*2)
+// console.log(numeros)
+// console.log(valoresDobrados)
+
+
+//exercicio: separar as idades e separar o foroff 
+
+
+// const usuarios = [
+// {nome:"João", idade:32},
+// {nome:"Maria", idade:15},
+// {nome:"Pedro", idade:21}
+// ]
+// const idadeUsuarios = usuarios.map(usuario => usuario.idade)
+// console.log(idadeUsuarios)
+// for(let idade of idadeUsuarios){
+//     console.log(idade)
+// }
+
+
+
+//versão contraida 
+// const usuarios = [
+// {nome:"João", idade:32},
+// {nome:"Maria", idade:15},
+// {nome:"Pedro", idade:21}
+// ]
+// for (let usuario of usuarios) {
+//   console.log(`${usuario.nome} tem ${usuario.idade} anos.`);
+// }
+
+//versão completa:
+
+//  const usuarios = [
+// {nome:"João", idade:32},
+// {nome:"Maria", idade:15},
+// {nome:"Pedro", idade:21}
+// ];
+// for (let usuario of usuarios) {
+//   console.log(usuario.nome + " tem " + usuario.idade + " anos.");
+// }
+
